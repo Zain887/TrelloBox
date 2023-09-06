c0990a608c9679749f250be5b84d1d239c0ee093
muhammad zain <zainansari887@gmail.com>
2023-09-06 13:49:34 +0500

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -18,7 +18,8 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <h1 className=' text-teal-600 text-9xl'>hello</h1>
     </div>
   );
 }
